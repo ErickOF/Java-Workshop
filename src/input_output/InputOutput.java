c215e0a23154e988250b99c94b9cf4147c99ca3d
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-08-23 11:05:22 -0600

    [Update] - Inputs/Outputs

---
+++ src/input_output/InputOutput.java
@@ -0,0 +1,23 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        // Creating a scanner instance
+        // System.in is a standard input stream
+        Scanner scanner = new Scanner(System.in);
+
+        // Integers
+        System.out.println("Enter an integer number: ");
+        int intNumber = scanner.nextInt();
+        System.out.println("Number: " + intNumber + "\n");
+
+        // Strings
+        System.out.println("Enter your name: ");
+        String name = scanner.next();
+        System.out.println("Hi " + name + "!\n");
+
+        System.out.println("Your name is " + name + " and you're " + intNumber + "yo");
+    }
+}
